954c13cdaf74f6d6c1a9625b4d83a0cbed629cec
xy.kong <xy.kong@gmail.com>
2024-11-18 00:56:19 +0800

    Bump version: 0.0.1 → 0.1.0

--- poetry_versions_plugin/versions.py
+++ poetry_versions_plugin/versions.py
@@ -1,8 +1,8 @@
 # Auto-generated version info
 
-branch = 'develop'
-commit = 'ddafc67'
-commit_count = 6
+branch = 'release/0.1.0'
+commit = '0694d2e'
+commit_count = 7
 is_dirty = True
-datetime = '2024-11-18 00:52:03'
-version = '0.0.1'
+datetime = '2024-11-18 00:56:19'
+version = '0.1.0'
