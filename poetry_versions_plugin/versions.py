ddafc679f94c1870da3a7304cb90b6d7261fb452
xy.kong <xy.kong@gmail.com>
2024-11-18 00:09:37 +0800

    reset pyproject version to 0.0.1

--- poetry_versions_plugin/versions.py
+++ poetry_versions_plugin/versions.py
@@ -1,8 +1,8 @@
 # Auto-generated version info
 
 branch = 'develop'
-commit = '6ffa675'
-commit_count = 4
+commit = '3fb6355'
+commit_count = 5
 is_dirty = True
-datetime = '2024-11-17 23:46:29'
-version = '0.6.0'
+datetime = '2024-11-18 00:08:47'
+version = '0.0.1'
