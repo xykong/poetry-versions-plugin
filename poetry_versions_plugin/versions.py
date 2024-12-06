23eee9b393382f771bbe4cf589b61cfb190bb754
xy.kong <xy.kong@gmail.com>
2024-12-06 18:19:55 +0800

    Bump version: 0.4.0 → None

--- poetry_versions_plugin/versions.py
+++ poetry_versions_plugin/versions.py
@@ -1,12 +1,12 @@
 # THIS FILE IS GENERATED DURING PROJECT BUILD
 # See poetry poetry-versions-plugin for details
 
-branch = 'release/0.4.0'
-commit = '3cf82c0'
-commit_count = 54
+branch = 'release/0.5.0'
+commit = 'fdae5e8'
+commit_count = 58
 is_dirty = False
-datetime = '2024-12-02 20:43:54'
-version = '0.4.0'
+datetime = '2024-12-06 18:19:55'
+version = '0.5.0'
 
 full_version = f'{version}.{branch}+{commit_count}.{commit}'
 # END OF GENERATED CODE
