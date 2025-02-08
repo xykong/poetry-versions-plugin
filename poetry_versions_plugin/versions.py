1d7c475ba5896b1d4aac1d0aeb8c2c77e683eb59
xy.kong <xy.kong@gmail.com>
2025-02-08 14:40:20 +0800

    Bump version: 0.6.1 → 0.7.0

--- poetry_versions_plugin/versions.py
+++ poetry_versions_plugin/versions.py
@@ -1,12 +1,12 @@
 # THIS FILE IS GENERATED DURING PROJECT BUILD
 # See poetry poetry-versions-plugin for details
 
-branch = 'release/0.6.1'
-commit = 'db88c4c'
-commit_count = 68
+branch = 'release/0.7.0'
+commit = 'c5fd192'
+commit_count = 72
 is_dirty = False
-datetime = '2025-02-08 14:31:08'
-version = '0.6.1'
+datetime = '2025-02-08 14:40:20'
+version = '0.7.0'
 
 full_version = f'{version}.{branch}+{commit_count}.{commit}'
 # END OF GENERATED CODE
