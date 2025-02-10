d5287084837de36edcd57e87aa4e0f4787b05b22
xy.kong <xy.kong@gmail.com>
2025-02-10 15:40:52 +0800

    Bump version: 0.9.0 → 0.9.1

--- poetry_versions_plugin/versions.py
+++ poetry_versions_plugin/versions.py
@@ -1,12 +1,12 @@
 # THIS FILE IS GENERATED DURING PROJECT BUILD
 # See poetry poetry-versions-plugin for details
 
-branch = 'release/0.9.0'
-commit = 'cba23bb'
-commit_count = 92
+branch = 'master'
+commit = '575106a'
+commit_count = 94
 is_dirty = False
-datetime = '2025-02-10 11:33:04'
-version = '0.9.0'
+datetime = '2025-02-10 15:40:52'
+version = '0.9.1'
 
 full_version = f'{version}.{branch}+{commit_count}.{commit}'
 # END OF GENERATED CODE
